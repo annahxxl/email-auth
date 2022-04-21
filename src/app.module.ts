b19f1fe83a525a7ec653ea1741a9423044c2907f
Hanna Lee <8annahxxl@gmail.com>
2022-04-21 17:16:37 +0900

    add: 예외 필터 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import authConfig from './config/authConfig';
 import emailConfig from './config/emailConfig';
 import { validationSchema } from './config/validationSchema';
 import { UsersModule } from './users/users.module';
+import { ExceptionModule } from './exception/exception.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UsersModule } from './users/users.module';
       validationSchema,
     }),
     TypeOrmModule.forRoot(),
+    ExceptionModule,
   ],
   controllers: [],
   providers: [],
